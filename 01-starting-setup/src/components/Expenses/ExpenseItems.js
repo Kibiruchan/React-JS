238f812ed0bb1dc7df293aa56d64c6a21b7b09b9
Falgueras del Alamo, Elena <elena.falgueras.del.alamo@tk.de>
2023-07-28 16:17:23 +0200

    ExpensesFilter

--- 01-starting-setup/src/components/Expenses/ExpenseItems.js
+++ 01-starting-setup/src/components/Expenses/ExpenseItems.js
@@ -1,23 +1,18 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI//Card';
 import './ExpenseItems.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.expense.title);
 
-  const clickHandler = () => {
-    setTitle('Canco!');
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.expense.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
       </div>
       <div className="expense-item__price">${props.expense.amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,14 +1,26 @@
+import React from "react"
 import ExpenseItem from './ExpenseItems'
 import Card from '../UI/Card'
 import './Expenses.css'
+import ExpensesFilter from './ExpensesFilter';
 
 function Expenses(props){
-    return(
-        <Card className='expenses'>
-            <ExpenseItem expense = {props.expenses[0]}></ExpenseItem>
-            <ExpenseItem expense = {props.expenses[1]}></ExpenseItem>
-            <ExpenseItem expense = {props.expenses[2]}></ExpenseItem>
+    const [filter, setFilter] = React.useState("0");
+
+    const filterChangeHandler = (event) =>{
+        console.log(event.target.value);
+        setFilter(event.target.value);
+    };
+
+    return (
+      <div>        
+        <Card className="expenses">
+          <ExpensesFilter selected={filter} onFilterChange = {filterChangeHandler}/>  
+          <ExpenseItem expense={props.expenses[0]}></ExpenseItem>
+          <ExpenseItem expense={props.expenses[1]}></ExpenseItem>
+          <ExpenseItem expense={props.expenses[2]}></ExpenseItem>
         </Card>
+      </div>
     );
 }
 
